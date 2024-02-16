9b91213d24c458e0a53673386c947fcdcc57c834
dor shaul <dor.asti@gmail.com>
2024-02-16 22:46:12 +0200

    adjustments

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,12 @@
 import { NavLink, useLocation } from "react-router-dom";
 import logo from "../../assets/logo.svg";
-import "./Header.scss";
+import styles from "./Header.module.scss";
 import SeperatorLine from "../SeperatorLine";
+import classNames from "classnames";
+import Switch from "../Switch";
+import { Icon } from "../Icon";
+import { useAtom } from "jotai";
+import { globalAtoms } from "../../state/globalAtoms";
 
 const Header = () => {
   const location = useLocation();
@@ -11,22 +16,39 @@ const Header = () => {
     { path: "/about", name: "about me" },
   ];
 
+  const [appTheme, setAppTheme] = useAtom(globalAtoms.appTheme);
+
+  const handleSelectTheme = () => {
+    const newTheme = appTheme === "dark" ? "light" : "dark";
+    setAppTheme(newTheme);
+    localStorage.setItem("appTheme", newTheme);
+  };
+
   return (
-    <div className="header">
-      <div className="header-content">
-        <div className="logo">
+    <div className={styles.header}>
+      <div className={styles.headerContent}>
+        <div className={styles.logo}>
           <NavLink to={"/"}>
             <img src={logo} alt="Logo" />
           </NavLink>
         </div>
-        <div className="navigations">
+        <div className={styles.navigations}>
+          <div className={styles.darkModeSwitch}>
+            <Icon name="sun" />
+            <Switch
+              isChecked={appTheme === "dark"}
+              onChange={() => handleSelectTheme()}
+            />
+            <Icon name="moon" />
+          </div>
           {navLinks.map((link, index) => (
             <NavLink
               key={index}
               to={link.path}
-              className={`nav-item ${
-                location.pathname === link.path && "active"
-              }`}
+              className={classNames(
+                styles.navItem,
+                location.pathname === link.path && styles.active
+              )}
             >
               {link.name}
             </NavLink>
