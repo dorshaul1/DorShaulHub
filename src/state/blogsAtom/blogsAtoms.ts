848fe33b01b4e6a595b2e71b93bcd5c4101b83de
dor shaul <dor.asti@gmail.com>
2024-04-26 14:37:20 +0300

    order the bllogsatom

--- src/state/blogsAtom/blogsAtoms.ts
+++ src/state/blogsAtom/blogsAtoms.ts
@@ -2,31 +2,40 @@ import { atom } from "jotai";
 import { initialFilters } from "./initializations";
 import { filterBlogs, sortBlogs } from "./utils";
 
-// Atoms
-const blogs = atom<TBlog[]>([]);
-const currentFilters = atom<TFilters>(initialFilters);
+// Blog Atoms
+const blogsAtom = atom<TBlog[]>([]);
+const currentFiltersAtom = atom<TFilters>(initialFilters);
 
-const filteredBlogs = atom<TBlog[]>((get) => {
-  const initialBlogs = get(blogs);
-  const filters = get(currentFilters);
+// Filtered Blogs
+const filteredBlogsAtom = atom((get) => {
+  const blogs = get(blogsAtom);
+  const filters = get(currentFiltersAtom);
 
-  const { sortBy, search, labels } = filters || {};
+  const { sortBy = "", search = "", labels = [] } = filters;
 
-  if (!search?.length && !labels?.length) return initialBlogs;
+  // If no search or labels, return the blogs without additional filtering
+  if (!search && labels.length === 0) {
+    return sortBlogs(blogs, sortBy);
+  }
 
-  const sortedBlogs = sortBlogs(initialBlogs, sortBy || "");
-  const filteredBlogs = filterBlogs(sortedBlogs, filters || {});
+  const sortedBlogs = sortBlogs(blogs, sortBy);
+  const filteredBlogs = filterBlogs(sortedBlogs, { search, labels });
 
   return filteredBlogs;
 });
 
-const latestBlogs = atom<TBlog[]>((get) => {
+// Latest Blogs
+const latestBlogsAtom = atom((get) => {
+  const blogs = get(blogsAtom);
+  const sortedBlogs = sortBlogs(blogs, "date");
+
   const NUMBER_OF_BLOGS = 3;
-
-  const all_blogs = get(blogs);
-  const sortedBlogs = sortBlogs(all_blogs, "date");
-
   return sortedBlogs.slice(0, NUMBER_OF_BLOGS).reverse();
 });
 
-export const blogsAtoms = { blogs, currentFilters, filteredBlogs, latestBlogs };
+export const blogsAtoms = {
+  blogs: blogsAtom,
+  currentFilters: currentFiltersAtom,
+  filteredBlogs: filteredBlogsAtom,
+  latestBlogs: latestBlogsAtom,
+};
