36550c8e717457b8782beb9f71b66dadd9b7bcf7
dor shaul <dor.asti@gmail.com>
2024-08-04 19:24:05 +0300

    add sticky style onScrolling

--- src/components/LatestBlogs/LatestBlogs.tsx
+++ src/components/LatestBlogs/LatestBlogs.tsx
@@ -3,27 +3,49 @@ import { Icon } from "../Icon";
 import { useAtomValue } from "jotai";
 import { blogsAtoms } from "../../state/blogsAtom/blogsAtoms";
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
 
 const LatestBlogs = () => {
   const latestBlogs = useAtomValue(blogsAtoms.latestBlogs);
 
-  return (
-    <div className={styles.topBlogs}>
-      <div className={styles.header}>
-        <h1 className={styles.title}>Latest Blogs</h1>
-      </div>
+  useEffect(() => {
+    const latestBlogsElement = document.getElementById("latest__blogs");
 
-      <div className={styles.list}>
-        {latestBlogs.map((blog: TBlog) => (
-          <Link to={`/blogs/${blog.id}`} className={styles.blog} key={blog?.id}>
-            <Icon
-              className={styles.icon}
-              color="var(--clr-primary)"
-              name="arrowRight"
-            />
-            <h3>{blog?.title}</h3>
-          </Link>
-        ))}
+    const onScroll = () => {
+      if (latestBlogsElement) {
+        latestBlogsElement.classList.toggle(styles.fixed, window.scrollY > 200);
+      }
+    };
+
+    window.addEventListener("scroll", onScroll);
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  }, []);
+
+  return (
+    <div className={styles.wrapper}>
+      <div id="latest__blogs" className={styles.latestBlogs}>
+        <div className={styles.header}>
+          <h1 className={styles.title}>Latest Blogs</h1>
+        </div>
+
+        <div className={styles.list}>
+          {latestBlogs.map((blog: TBlog) => (
+            <Link
+              to={`/blogs/${blog.id}`}
+              className={styles.blog}
+              key={blog?.id}
+            >
+              <Icon
+                className={styles.icon}
+                color="var(--clr-primary)"
+                name="arrowRight"
+              />
+              <h3>{blog?.title}</h3>
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
